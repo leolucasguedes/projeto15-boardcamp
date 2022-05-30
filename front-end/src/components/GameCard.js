85194aad557f6d0b0738a65fb1967fddfc7955cb
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-05-30 09:38:49 -0300

    bugfix: gitignore front-end

--- front-end/src/components/GameCard.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export default function GameCard ({ game, rentInfo }) {
-  return (
-    <div>lalala</div>
-  );
-}
--- front-end/src/components/View.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const PageView = styled.main`
-  display: flex;
-`;
-
-export default PageView;
